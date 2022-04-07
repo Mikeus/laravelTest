bfbcddb53c21d33c157985582bac7b06ff497dc1
Michal Ziembicki <vipmikeus@gmail.com>
2022-04-07 18:30:16 -0400

    Route parameters Query Strings and groups

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,27 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Http\Request;
 
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/about', function () {
-    return view('pages/about');
+
+Route::prefix('/blog/new')-> group(function(){
+   Route::get('/march/{title}', function (Request $request, $title) {
+    // dd($request -> query("max"));
+    return "<h1>{$title} {$request -> query("min")} : {$request -> query("max")}</h1>";
+}); 
+   Route::get('/april/{title}', function (Request $request, $title) {
+    // dd($request -> query("max"));
+    return "<h1>{$title} {$request -> query("min")} : {$request -> query("max")}</h1>";
+}); 
+   Route::get('/about', function () {
+
+    return "<h1>About</h1>";
 });
-Route::get('/pokemon/my', function () {
-    return "helooooo";
-    // [
-    //     "id" => 1,
-    //     "range" => "Pikachy",
-    //     "power" => "Electric",
-    // ];
 });
+
+
 
